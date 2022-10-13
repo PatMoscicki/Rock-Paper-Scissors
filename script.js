75171f8d6390ffc9d77fc539e90f40bd8e9be02d
Patryk Mościcki <patryk.moscicki@gmail.com>
2022-10-13 14:46:02 +0200

    Add prompt for player's choice / add pseudocode for Game function

--- script.js
+++ script.js
@@ -18,45 +18,55 @@ let choice = ["Rock", "Paper", "Scissors"][Math.floor(Math.random()*3)];
 return choice;
 }
 
+// Player selection function with prompt - case insensitive
+const answer = prompt("Make your choice: rock, paper or scissors", "");
+let playerSelection = answer[0].toUpperCase() + answer.slice(1).toLowerCase();
+const computerSelection = getComputerChoice();
+
 // Function that allows a single round to be played - neither an optimal nor an elegant solution but it works
 
+
+
+
 function playRound(playerSelection, computerSelection) {
+
      if (playerSelection == "Scissors" && computerSelection == "Paper") {
-      console.log("Scissors beat paper, You win!");
+      return "Scissors beat paper, You win!";
     }
 
     else if (playerSelection == "Scissors" && computerSelection == "Rock") {
-        console.log("Rock beats scissors, You lose!")
+        return "Rock beats scissors, You lose!"
     }
 
     else if (playerSelection == "Rock" && computerSelection == "Scissors") {
-        console.log("Rock beats scissors, You win!");
+        return "Rock beats scissors, You win!";
     }
 
     else if (playerSelection == "Rock" && computerSelection == "Paper") {
-        console.log("Paper beats rock, You lose!")
+        return "Paper beats rock, You lose!";
     }
 
     else if (playerSelection == "Paper" && computerSelection == "Rock") {
-        console.log("Paper beats rock, You win!");
+       return "Paper beats rock, You win!";
     }
 
     else if (playerSelection == "Paper" && computerSelection == "Scissors") {
-        console.log("Scissors beat paper, You lose!");
+        return "Scissors beat paper, You lose!";
     }
 
     else {
-        console.log("Tie, Let's do it again!");
+        return "Tie, Let's do it again!";
     }
 
 }
 
-// Player selection function - case insensitive
 
-const answer = "scissors";
-let playerSelection = answer[0].toUpperCase() + answer.slice(1).toLowerCase();
 
-const computerSelection = getComputerChoice();
-
+// Result display in console (playRound)
 console.log(playRound(playerSelection, computerSelection));
+
+// function game:
+// plays 5 rounds of playRound
+// lists all results returned from each of the 5 rounds played
+// displays results in console
 
