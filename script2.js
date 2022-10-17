4a84e1829b8a02b4bea8bf842461ef6eff275048
Patryk Mościcki <patryk.moscicki@gmail.com>
2022-10-17 11:53:57 +0200

    Refactor code / add working score tracker / Add alert for final results of the game

--- script2.js
+++ script2.js
@@ -1,8 +1,9 @@
+// Initialization of point values before the game starts
+let playerScore = 0;
+let computerScore = 0;
 
-
-
+// Function that loops five rounds of the game
 function game() {
-
     for (let i = 0; i < 5; i++) {
 
 // Computer choice randomizer
@@ -22,54 +23,84 @@ const playerSelection = answer[0].toUpperCase() + answer.slice(1).toLowerCase();
 
 
 
-// Play one round of the game:
+// Play one round of the game, calculate results of each round:
 
 function playRound(playerSelection, computerSelection) {
-  
-   if (playerSelection == computerSelection){
+    
+   if (playerSelection == computerSelection) {
         return "Draw, try again"; 
     }
 
     else if (playerSelection == "Rock") {
         if (computerSelection == "Scissors") {
+            playerScore += 1;
             return "Rock wins, congrats";
         }
 
         else {
+            computerScore += 1;
             return "Paper wins, too bad";
         }
+
     }
 
     else if (playerSelection == "Paper") {
         if (computerSelection == "Rock") {
+            playerScore += 1;
             return "Paper wins, congrats"
         }
 
         else {
+            computerScore += 1;
             return "Scissors win, too bad";
         }
     }
 
     else if (playerSelection == "Scissors") {
+        
         if (computerSelection == "Paper") {
+            playerScore += 1;
             return "Scissors win, congrats";
+            
         }
 
         else {
+            computerScore += 1;
             return "Rock wins, too bad";
         }
     }
 
+ 
     else {
         return "Wrong choice, pick again";
     }
+ 
 }
 
-
 console.log(playRound(playerSelection, computerSelection));
+
     }
 }
 
-// Call game function
+// Function that calculates the final results of five rounds played
+
+function finalResult() {
+    if (playerScore > computerScore) {
+        return "Final result: You won the game";
+    }
+    else if (playerScore < computerScore) {
+        return "Final result: Computer won the game";
+    }
+
+    else {
+        return "Final result: Draw";
+    }
+}
+
+// Call game function, display final results
 
 game();
+console.log(finalResult());
+
+
+
